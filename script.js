e29897d3ba1b06df65ff2d1bd91a973abf2d4e50
lmcygnus <mloida591@gmail.com>
2024-01-30 13:57:51 -0300

    display mark

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-const gameboard = (() => {
-    let squares = ["","","","","","","","",""];
-    const displaySquares = document.querySelectorAll(".square");
-    console.log(displaySquares)
-    const setdata = () => {
-        squares.forEach((_, index) => {
-            displaySquares[index].setAttribute("data-number", index);
-            displaySquares[index].addEventListener("click", () => {
-                alert(displaySquares[index].dataset.number);
-            });
-        });
-    };
-    return {
-        setdata
-    };
-})();
-gameboard.setdata();
+const displaySquares = document.querySelectorAll(".square");
 const createPlayer = (name, mark) => {
     return {name, mark};
 }
 
 const game = (() => {
-    let players = [];
-    let currentPlayer;
-    const start = () => {
-        players = [createPlayer("x", "x"), createPlayer("o", "o")];
-        currentPlayer = 0;
-        gameboard.setId();
-    }
+    let player1 = createPlayer("x player", "x");
+    let player2 = createPlayer("o player", "o");
+    let activePlayer = player1;
+    
+    const switchTurn = () => {
+        activePlayer = (activePlayer === player1) ? player2 : player1;
+    };
 
-    return {start,};
+    const updateDisplay = () => {
+        displaySquares.forEach(square => {
+            square.addEventListener('click', () => {
+                if (square.textContent === "") {
+                    square.textContent = activePlayer.mark;
+                    switchTurn();
+                }
+            });
+        });
+    };
+
+    return { updateDisplay };
 })();
+
+game.updateDisplay();
+
+const gameboard = (() => {
+    let squares = ["","","","","","","","",""];
+    console.log(displaySquares)
+    const setdata = () => {
+        squares.forEach((_, index) => {
+            displaySquares[index].setAttribute("data-number", index);
+            displaySquares[index].addEventListener("click", game.updateDisplay());
+        });
+        
+    };
+    return {
+        setdata,
+    };s
+})();
+gameboard.setdata();
+
